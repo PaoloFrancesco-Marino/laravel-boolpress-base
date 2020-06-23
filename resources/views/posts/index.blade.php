afa1ecd417cd0445013e8e796b6afa48cdf3ff6a
Paolo Francesco Marino <paolo.francesco.marino@gmail.com>
2020-06-23 17:28:17 +0200

    add comment ot html with relationship posts

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -5,12 +5,21 @@
 
         <div> 
             @foreach ($posts as $post)
-                <ul class="list-group mb-5">
-                    <li class="list-group-item"><h4>{{ $post->user->name}}</h4></li>
+                <ul class="list-group mb-2">
+                    <li class="list-group-item"><h4 class="font-weight-bold">{{ $post->user->name}}</h4></li>
                     <li class="list-group-item"><h4>{{ $post->title }}</h4></li>
                     <li class="list-group-item">{{ $post->post }}</li>
                     <li class="list-group-item"><h5>Creato il {{ $post->created_at }}, Modificato il {{ $post->updated_at }}</h5></li>
                 </ul>
+                
+                <h5>Comments</h5>
+                <ul class="mb-5">
+                    @foreach ($post->comments as $comment)
+                        <h6 class="font-weight-bold">{{ $comment->name }}</h6>
+                        <p>{{ $comment->comment }}</p>
+                    @endforeach
+                </ul>
+                
             @endforeach
 
             <h4>Pagine</h4>
